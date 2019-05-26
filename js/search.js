41d3344c4bb8a73b1965137b4cb95479562abad7
Elwin Huaman <elwinlhq@hotmail.com>
2019-05-26 20:26:56 +0200

    SPARQL endpoint added

--- js/search.js
+++ js/search.js
@@ -1,9 +1,11 @@
 function validateForm(){
   var inputFrom = document.getElementById("from").value;
   var inputTo = document.getElementById("to").value;
+  var inputDays = document.getElementById('days').value;
+  var email = document.getElementById('email').value;
   validateEmpty(inputTo);
   validateEmpty(inputFrom);
-  sparqlQuery();
+  sparqlQuery(inputTo);
 }
 
 function validateEmpty(value){
@@ -11,13 +13,26 @@ function validateEmpty(value){
   console.log("The form has empty fields");
 }
 
-function sparqlQuery(){
+function sparqlQuery(inputTo){
   var dbpedia = new SPARQL({
     apikey: "YOUR-API-KEY-HERE",
-    endpoint: "https://dbpedia.org/sparql/"
+    // endpoint: "https://dbpedia.org/sparql/"
+    endpoint: "http://lod.cs.aau.dk:8891/sparql"
   });
 
-  var queryCountryName = "PREFIX  dbpedia-owl:  <http://dbpedia.org/ontology/>\n\
+  var queryCityNO2 = "PREFIX s: <http://qweb.cs.aau.dk/airbase/schema/>\n\
+                      PREFIX p: <http://qweb.cs.aau.dk/airbase/property/>\n\
+                      PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n\
+                      SELECT ?no2 ?city (avg(?no2) as ?avgNO2)\n\
+                      WHERE { \n\
+                        ?obs s:NO2 ?no2 . \n\
+                        ?obs s:year ?year . \n\
+                        ?obs s:station ?station . \n\
+                        ?station s:inCity ?city . \n\
+                        FILTER(?city = <http://qweb.cs.aau.dk/airbase/data/city/" + inputTo + "/>) \n\
+                        } LIMIT 10";
+
+  var queryCountryName = "PREFIX dbpedia-owl:  <http://dbpedia.org/ontology/>\n\
                           PREFIX dbpedia: <http://dbpedia.org/resource>\n\
                           PREFIX dbpprop: <http://dbpedia.org/property>\n\
                        SELECT DISTINCT ?citylabel ?countrylabel ?pop \n\
@@ -29,7 +44,8 @@ function sparqlQuery(){
                          ?city dbpedia-owl:populationTotal ?pop . \n\
                          FILTER ( lang(?countrylabel) = 'en' and lang(?citylabel) = 'en' and ?pop>10000) \n\
                       } LIMIT 10";
-  dbpedia.query(queryCountryName).done(onSuccess).error(onFailure);
+
+  dbpedia.query(queryCityNO2).done(onSuccess).error(onFailure);
 }
 function onFailure(xhr, status) {
    document.getElementById("result").innerHTML = status + " (See console.)";
--- js/sparql.js
+++ js/sparql.js
@@ -9,4 +9,13 @@ SPARQL = function(o) {
   };
 };
 
-
+// SPARQL = function(o) {
+//   this.query = function(q) {
+//     return $.ajax({
+//       url: o.endpoint,
+//       accepts: {json: "application/sparql-results+json"},
+//       data: {query: q, apikey: o.apikey},
+//       dataType: "json"
+//     });
+//   };
+// };
