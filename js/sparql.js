34acf7f8ec934da074752423217e4861f5768d08
Elwin Huaman <elwinlhq@hotmail.com>
2019-05-26 20:48:27 +0200

    http request header added

--- js/sparql.js
+++ js/sparql.js
@@ -2,6 +2,8 @@ SPARQL = function(o) {
   this.query = function(q) {
     return $.ajax({
       url: o.endpoint,
+      headers: {  'Access-Control-Allow-Origin': 'http://lod.cs.aau.dk:8891/sparql' },
+      crossDomain: true,
       accepts: {json: "application/sparql-results+json"},
       data: {query: q, apikey: o.apikey},
       dataType: "json"
